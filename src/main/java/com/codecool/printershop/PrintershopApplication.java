37e7fada9d7d4945de44a5fc40a1a9e594abccdc
Albi012 <mayeralbert12@gmail.com>
2020-09-21 13:23:52 +0200

    Initial commit

---
+++ src/main/java/com/codecool/printershop/PrintershopApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.printershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintershopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrintershopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/printershop/PrintershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.printershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintershopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
